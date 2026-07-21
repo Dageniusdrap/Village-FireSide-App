47464f062d6ff8e4c38773a89c082476301462ad
Dageniusdrap <dradrigapatrick@gmail.com>
2026-07-21 04:26:53 +0300

    chore: add shared eslint-config package and prettier config

---
+++ packages/eslint-config/base.js
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import tseslint from "typescript-eslint";
+
+/** @type {import("eslint").Linter.Config[]} */
+export const baseConfig = [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+    },
+  },
+  {
+    ignores: ["dist/**", ".next/**", ".expo/**", "node_modules/**"],
+  },
+];
